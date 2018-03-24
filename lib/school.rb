55994e11342354ec675a553751552c38ff43e290
Alexander Spanos <alexisspa9@gmail.com>
2018-03-24 23:28:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,6 +2,17 @@
 class School
   def initialize(name)
     @name = name
-    
+
+  end
+  def add_student(name,n)
+    @student_name = name
+    @student_number = n
+
+  end
+  def roster
+    @roster = {
+
+    }
+
   end
 end

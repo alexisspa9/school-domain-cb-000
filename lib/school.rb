d09996d2c6d346c5f7f2a3f07d62669c01f15c08
Alexander Spanos <alexisspa9@gmail.com>
2018-03-24 23:35:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,15 +4,14 @@ class School
     @name = name
 
   end
+  roster {}
   def add_student(name,n)
     @student_name = name
     @student_number = n
     @roster[@student_number] = []
     @roster[@student_number] << @student_name
   end
-  def roster
-    @roster = {}
-  end
+
   def grade(n)
     @roster.each.map do |roster|
       roster
